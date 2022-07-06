5e0d7e3347499f38813036672ca0694f624beb6e
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-06 18:46:31 +0900

    괄호 읽기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,19 +6,46 @@ import java.util.Scanner;
 public class Main extends Setting implements ActivityItem {
 
     public static void main(String[] args) {
-        new Main(args);
+        new Main();
     }
 
-    private Main(String[] args) {
+    private Main() {
         super.varClear();
         Scanner scanner = new Scanner(System.in);
+        int stack = 0;
+
         while (true) {
             try {
                 System.out.print(">>> ");
-                super.start(scanner.nextLine());
+                String line = scanner.nextLine();
+                stack += count(line, '{');
+
+                if (stack == 0) super.start(line);
+                else if (stack > 0) {
+                    total.append(line);
+                    while (stack > 0) {
+                        Scanner sc = new Scanner(System.in);
+                        System.out.print("--- ");
+                        String t = sc.nextLine();
+                        stack += count(t, '{');
+                        stack -= count(t, '}');
+                        total.append(t).append("\n");
+                    }
+                    for (String l : bracket.bracket(total.toString()).split("\\n"))
+                        super.start(l);
+                    total.setLength(0);
+                } else stack = 0;
             } catch (Exception e) {
-                e.printStackTrace();
+                System.err.println("\n" + e.getMessage());
             }
         }
+    }
+
+    private int count(String line, char c) {
+        int count = 0;
+        for (int i = 0; i<line.length(); i++) {
+            if (line.charAt(i) == c) count++;
+        }
+        return count;
     }
 }
