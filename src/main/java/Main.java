2322f1059cf95ec8f0c91a3bd92587be77a751f5
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-13 01:21:04 +0900

    메인 메소드 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,56 @@
-import origin.item.ActivityItem;
-import origin.item.Setting;
 
+import event.Setting;
+import http.items.Color;
+import http.server.Server;
+import origin.exception.FileFailException;
+import origin.exception.FileFailMessage;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
-public class Main extends Setting implements ActivityItem {
+public class Main extends Setting {
 
     public static void main(String[] args) {
-        new Main();
+        new Main(args);
     }
 
-    private Main() {
-        super.varClear();
-        Scanner scanner = new Scanner(System.in);
-        int stack = 0;
+    private Main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("OTLanguage 종료 됨\n")));
+        firstStart();
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("OTLanguage 종료 됨")));
+        if(args.length > 0) path = args[0];
+        if (args.length == 2) fileRead(path + "/" + args[1]);
+        else {
+            Scanner scanner = new Scanner(System.in);
+            int stack = 0;
+            while (true) {
+                try {
+                    System.out.print(">>> ");
+                    String line = scanner.nextLine();
+                    if (line.equalsIgnoreCase("exit")) break;
+                    stack += count(line, '{');
+                    if (stack == 0) super.start(line);
+                    else if (stack > 0) {
+                        total.append(line);
+                        while (stack > 0) {
+                            Scanner sc = new Scanner(System.in);
+                            System.out.print("--- ");
+                            String t = sc.nextLine();
+                            stack += count(t, '{');
+                            stack -= count(t, '}');
+                            total.append(t).append("\n");
+                        }
+                        for (String l : bracket.bracket(total.toString().replace("{", "{\n")).split("\\n"))
+                            super.start(l);
+                        total.setLength(0);
+                    } else stack = 0;
 
-        while (true) {
-            try {
-                System.out.print(">>> ");
-                String line = scanner.nextLine();
-                if (line.equalsIgnoreCase("exit")) break;
-                stack += count(line, '{');
-                if (stack == 0) super.start(line);
-                else if (stack > 0) {
-                    total.append(line);
-                    while (stack > 0) {
-                        Scanner sc = new Scanner(System.in);
-                        System.out.print("--- ");
-                        String t = sc.nextLine();
-                        stack += count(t, '{');
-                        stack -= count(t, '}');
-                        total.append(t).append("\n");
-                    }
-                    for (String l : bracket.bracket(total.toString().replace("{", "{\n")).split("\\n"))
-                        super.start(l);
-                    total.setLength(0);
-                } else stack = 0;
-
-            } catch (Exception e) {
-                System.err.println("\n" + e.getMessage());
+                } catch (Exception e) {
+                    System.err.println("\n" + e.getMessage());
+                }
             }
         }
     }
@@ -50,5 +61,29 @@ public class Main extends Setting implements ActivityItem {
             if (line.charAt(i) == c) count++;
         }
         return count;
+    }
+
+    private void fileRead(String filePath) {
+        if (!new File(filePath).canRead()) throw new FileFailException(FileFailMessage.doNotReadFile);
+        if (!filePath.toLowerCase(Locale.ROOT).endsWith(".otl")) throw new FileFailException(FileFailMessage.notMatchExtension);
+        String text;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath, StandardCharsets.UTF_8))) {
+            while ((text = reader.readLine()) != null) Setting.total.append(text).append("\n");
+            //괄호 -> 고유 아이디로 전환 //괄호 계산
+            String total = bracket.bracket(Setting.total.toString());
+            for (String line : total.split("\\n")) {
+                start(line); // 실행 메소드
+            }
+        } catch (IOException ignored) {}
+        pause();
+    }
+
+    private void pause() {
+        try {
+            System.in.read();
+        } catch (IOException ignored) {} finally {
+            if (Server.httpServerManager != null) Server.httpServerManager.stop();
+        }
+        System.exit(0);
     }
 }
