f4ad57e340173fb83c320d23fdf56ff68d86d9ae
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-13 01:15:11 +0900

    서버 로직 추가

--- src/main/java/http/server/RootHandler.java
+++ src/main/java/http/server/RootHandler.java
@@ -3,24 +3,23 @@ package http.server;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import http.items.Color;
-import http.items.Temporary;
+import http.handler.HttpGetHandler;
+import http.handler.HttpPostHandler;
+import http.items.HttpRepository;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-public class RootHandler implements HttpHandler, Temporary {
+public class RootHandler implements HttpHandler, HttpRepository {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String path = exchange.getRequestURI().getPath();
-        try (OutputStream responsive = exchange.getResponseBody()) {
-            if (Temporary.pathMap.containsKey(path)) {
-                String filePath = new CreateHTML().changeVariable(Temporary.pathMap.get(path));
+        try (exchange; OutputStream responsive = exchange.getResponseBody()) {
+            if (HttpRepository.pathMap.containsKey(path)) {
+                String filePath = new CreateHTML().changeVariable(HttpRepository.pathMap.get(path));
                 ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(filePath);
 
                 int contentLen = byteBuffer.limit();
@@ -31,13 +30,14 @@ public class RootHandler implements HttpHandler, Temporary {
                 headers.add("Content-Type", "text/html;charset=UTF-8");
                 headers.add("Content-Length", String.valueOf(contentLen));
 
-                //[2022-01-01 01:01:12][GET] [PATH] /test | [QueryString] username=Test
-                String dateFormat = "yyyy-MM-dd H:mm:ss";
+                String method = exchange.getRequestMethod();
                 String query = exchange.getRequestURI().getQuery();
-                System.out.printf("[%s]", new SimpleDateFormat(dateFormat).format(new Date()));
-                System.out.printf("%s[%s]%s", Color.cyan, exchange.getRequestMethod(), Color.exit);
-                System.out.printf(" %s[PATH]%s %s ", Color.blue, Color.exit, path == null ? "" : path);
-                System.out.printf("| %s[QueryString]%s %s\n", Color.blue, Color.exit, query == null ? "" : query);
+
+                String value;
+                if (method.equals("POST")) value = new HttpPostHandler().handle(exchange);
+                else value = new HttpGetHandler().handle(exchange);
+
+                printLog(method, path, value);
 
                 exchange.sendResponseHeaders(200, contentLen);
                 responsive.write(content);
@@ -45,8 +45,6 @@ public class RootHandler implements HttpHandler, Temporary {
             } else {
                 if (!path.equals("/favicon.ico")) System.out.println(path + "가 정의되어 있지 않습니다.");
             }
-        } finally {
-            exchange.close();
         }
     }
 }
--- src/main/java/http/server/Server.java
+++ src/main/java/http/server/Server.java
@@ -1,32 +1,25 @@
 package http.server;
 
 import http.items.Color;
-import http.items.Temporary;
+import http.items.HttpRepository;
 
-public class Server implements Temporary {
+public class Server implements HttpRepository {
+    public static HttpServerManager httpServerManager;
 
     public Server() {
-        HttpServerManager httpServerManager = new HttpServerManager();
-        try {
-            httpServerManager.start();
-            if (pathMap.isEmpty()) System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.red, Color.exit);
-            else {
-                while (true) {}
-            }
-        } finally {
+        httpServerManager = new HttpServerManager();
+        httpServerManager.start();
+        if (pathMap.isEmpty()) {
+            System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.RED, Color.RESET);
             httpServerManager.stop();
         }
     }
 
     public Server(int port) {
-        HttpServerManager httpServerManager = new HttpServerManager(port);
-        try {
-            httpServerManager.start();
-            if (pathMap.isEmpty()) System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.red, Color.exit);
-            else {
-                while (true) {}
-            }
-        } finally {
+        httpServerManager = new HttpServerManager(port);
+        httpServerManager.start();
+        if (pathMap.isEmpty()) {
+            System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.RED, Color.RESET);
             httpServerManager.stop();
         }
     }
