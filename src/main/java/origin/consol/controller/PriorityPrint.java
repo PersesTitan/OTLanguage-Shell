1776a352d6034c49b448844bafebe1992540fca2
PersesTitan <torampersesTitan@gmail.com>
2022-08-29 00:28:44 +0900

    Update PriorityPrint.java

--- src/main/java/origin/consol/controller/PriorityPrint.java
+++ src/main/java/origin/consol/controller/PriorityPrint.java
@@ -1,12 +1,18 @@
 package origin.consol.controller;
 
 import origin.consol.define.PriorityPrintWork;
+import origin.variable.model.Repository;
 
 import java.util.regex.Pattern;
 
 public class PriorityPrint implements PriorityPrintWork {
-    private final String PATTERN = "^\\s*!ㅅㅁㅅ!(\\s|$)";
-    private final Pattern pattern = Pattern.compile(PATTERN);
+    private final String PATTERN;
+    private final Pattern pattern;
+
+    public PriorityPrint(String patternText) {
+        this.PATTERN = "^\\s*"+patternText+"(\\s|$)";
+        this.pattern = Pattern.compile(this.PATTERN);
+    }
 
     @Override
     public void start(String line) {
