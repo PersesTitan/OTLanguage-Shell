7def96cd0281790c0c3be72b40e01e371f2c6009
PersesTitan <torampersesTitan@gmail.com>
2022-08-30 07:59:55 +0900

    Update HttpGetPost.java

--- src/main/java/http/controller/HttpGetPost.java
+++ src/main/java/http/controller/HttpGetPost.java
@@ -1,40 +1,47 @@
 package http.controller;
 
 import http.items.HttpRepository;
-import http.server.Server;
-import origin.exception.ServerException;
-import origin.exception.ServerMessage;
+import origin.exception.MatchException;
+import origin.exception.MatchMessage;
+import origin.loop.model.LoopWork;
 
-import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 //GET 과 POST 값 가져오는 작업
-public class HttpGetPost implements HttpRepository {
+public class HttpGetPost implements HttpRepository, LoopWork {
     //[경로] [POST,GET]>ㅇㅅㅇ>[name]
-    private final String patternText = "\\S+\\s+(POST|GET)\\s*>ㅇㅅㅇ>\\s*\\S+";
+    //[POST,GET][경로]>ㅇㅅㅇ>[변수1:query1, 변수2:query2]
+
+//    private final String patternText = "\\S+\\s+(POST|GET)\\s*>ㅇㅅㅇ>\\s*\\S+";
+    private final String patternText = "^\\s*(POST|GET)\\s*\\S+\\s*>ㅇㅅㅇ>\\s*\\[([^\\[\\]:,]+:[^\\[\\]:,]+)+]";
     private final Pattern pattern = Pattern.compile(patternText);
 
+    @Override
     public boolean check(String line) {
         return pattern.matcher(line).find();
     }
 
-    public String start(String line) {
-        if (Server.httpServerManager == null) throw new ServerException(ServerMessage.nullServer);
+    @Override
+    public void start(String line) {
         Matcher matcher = pattern.matcher(line);
-
-        while (matcher.find()) {
-            String text = matcher.group();
-            var texts = text.split(">ㅇㅅㅇ>");
-            StringTokenizer pathAndMethod = new StringTokenizer(texts[0].strip()); // [path] [POST, GET]
-            String name = texts[1].strip(); // [name]
-            String path = pathAndMethod.nextToken(); // [path]
-            String method = pathAndMethod.nextToken(); // [POST, GET]
-
-            String postMethod = "POST";
-            if (method.equals(postMethod)) line = line.replaceFirst(patternText, POST.get(path).getOrDefault(name, "").toString());
-            else line = line.replaceFirst(patternText, GET.get(path).getOrDefault(name, "").toString());
+        if (matcher.find()) {
+            String[] group = matcher.group().strip().split(">ㅇㅅㅇ>");
+            if (group.length != 2) throw new MatchException(MatchMessage.grammarError);
+            String path = group[0].trim(); // POST/sub
+            String variable = group[1].trim() + line.replaceFirst(patternText, ""); //[변수1:query1, 변수2:query2] id
+            if (path.startsWith("POST")) {
+                path = path.replaceFirst("^POST", "").trim();
+                POST.put(path, variable);
+            } else if (path.startsWith("GET")) {
+                path = path.replaceFirst("^GET", "").trim();
+                GET.put(path, variable);
+            }
         }
-        return line;
+    }
+
+    @Override
+    public String getPattern() {
+        return patternText;
     }
 }
