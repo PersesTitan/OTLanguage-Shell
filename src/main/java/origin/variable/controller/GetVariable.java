360cc79872ecb3849f4bd574e96753dd3126fed9
PersesTitan <torampersesTitan@gmail.com>
2022-09-02 00:49:44 +0900

    Update GetVariable.java

--- src/main/java/origin/variable/controller/GetVariable.java
+++ src/main/java/origin/variable/controller/GetVariable.java
@@ -1,24 +1,29 @@
 package origin.variable.controller;
 
 import http.items.HttpRepository;
+import origin.exception.MatchException;
+import origin.exception.MatchMessage;
+import origin.exception.VariableException;
+import origin.exception.VariableMessage;
 import origin.variable.model.Repository;
 
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 //변수를 불러와서 대치하는 작업
-public class GetVariable implements Repository {
-    //:변수명( )
-    String patternText = ":[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9_-]+[ _]";
-    Pattern pattern = Pattern.compile(patternText);
+public class GetVariable {
+    public static final String patternText = ":[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9_-]+[ _]"; //:변수명( )
+    private final Pattern pattern = Pattern.compile(patternText);
 
     public boolean check(String line) {
+        Pattern pattern = Pattern.compile("[^:]?"+patternText);
         return pattern.matcher(line).find();
     }
 
-    public String start(String line) {
+    public String start(String line, Map<String, Map<String, Object>> repository) {
         Matcher matcher = pattern.matcher(line);
-        if (matcher.find()) {
+        while (matcher.find()) {
             String group = matcher.group();
             int pos = group.length();
             String key = group.substring(1, pos-1); //:변수명 => 변수명
@@ -29,7 +34,6 @@ public class GetVariable implements Repository {
                     line = line.replaceAll(patternText, HttpRepository.partMap.get(key));
             }
         }
-
         return line;
     }
 }
